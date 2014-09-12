3bac1252b28592533b26bbb153bb51ca3fcaf049
Jose Prous <joseprous@gmail.com>
2014-09-12 13:44:20 -0400

    pass interface as parameter

--- ankercfg.c
+++ ankercfg.c
@@ -4,24 +4,22 @@
 #include <string.h>
 #include <libusb-1.0/libusb.h>
 
-#define INTERFACE 2
-
-void end_usb(libusb_device_handle* device)
+void end_usb(libusb_device_handle* device, int interface)
 {
-    libusb_release_interface(device,INTERFACE);
-    libusb_attach_kernel_driver(device,INTERFACE);
+    libusb_release_interface(device,interface);
+    libusb_attach_kernel_driver(device,interface);
     libusb_close(device);
     libusb_exit(NULL);    
 }
 
-void error(libusb_device_handle* device,char *message, int error_code, int err_no)
+void error(libusb_device_handle* device, int interface,char *message, int error_code, int err_no)
 {
     printf("%s (%s)(%s)\n",message, libusb_strerror(error_code), strerror(err_no));
-    end_usb(device);
+    end_usb(device, interface);
     exit(error_code);    
 }
 
-libusb_device_handle* init_usb(uint16_t vendor_id, uint16_t product_id)
+libusb_device_handle* init_usb(uint16_t vendor_id, uint16_t product_id, int interface)
 {
     int r;
     libusb_device_handle* device;
@@ -40,17 +38,17 @@ libusb_device_handle* init_usb(uint16_t vendor_id, uint16_t product_id)
         exit(-1);
     }
 
-    libusb_detach_kernel_driver(device,INTERFACE);
+    libusb_detach_kernel_driver(device,interface);
 
-    r = libusb_claim_interface(device,INTERFACE);
+    r = libusb_claim_interface(device,interface);
     if (r < 0){
-        error(device,"Error claim interfase",r,errno);
+        error(device,interface,"Error claim interfase",r,errno);
     }
     
     return device;
 }
 
-void change_color(libusb_device_handle* device, unsigned char cyan,unsigned char magenta, unsigned char yellow)
+void change_color(libusb_device_handle* device, int interface, unsigned char cyan,unsigned char magenta, unsigned char yellow)
 {
     unsigned char data[16] = {0x02,0x04,0xff,0xff,0xff,0x01,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
     int r;
@@ -58,7 +56,7 @@ void change_color(libusb_device_handle* device, unsigned char cyan,unsigned char
     unsigned char bRequest = 9;
     uint16_t wValue = 0x0302;
     uint16_t wIndex = 2;
-	uint16_t wLength = 16;
+    uint16_t wLength = 16;
     unsigned int timeout = 0;
     
     data[2] = cyan;
@@ -68,7 +66,7 @@ void change_color(libusb_device_handle* device, unsigned char cyan,unsigned char
 //    r = libusb_control_transfer (device, 0x21, 9, 0x0302, 2, data, 16, 0);
     r = libusb_control_transfer (device, request_type, bRequest, wValue, wIndex, data, wLength, timeout);
     if (r < 0){
-        error(device,"Error in transfer",r,errno);
+        error(device,interface,"Error in transfer",r,errno);
     }
 
 }
@@ -78,14 +76,15 @@ int main()
 {
     uint16_t vendor_id = 0x04d9;
     uint16_t product_id = 0xfa50;
+    int interface = 2;
     libusb_device_handle* device;    
 
-    device = init_usb(vendor_id,product_id);
+    device = init_usb(vendor_id, product_id, interface);
     
     //change_color(device,0xf0,0xf0,0x42); //blue
-    change_color(device,0xff,0xff,0x00); 
+    change_color(device,interface,0xff,0xff,0x00); 
 
-    end_usb(device);
+    end_usb(device, interface);
     
     return 0;
 }
